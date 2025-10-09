4922ac7ed74dc8c3a666431ef7cbd6c24643c606
Joseph Ch <josepch.1057@gmail.com>
2025-10-09 17:09:38 +0000

    reduce the text size a bit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -54,7 +54,7 @@ export default function AboutPage() {
     <div className="flex flex-col bg-background">
       <header className="h-[80vh] flex items-center justify-center text-center bg-black">
         <div className="container">
-          <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-bold tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-purple-600 to-purple-800 animate-gradient">
+          <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-purple-600 to-purple-800 animate-gradient">
             We are Acknowledgement Hub
           </h1>
           <p className="mx-auto max-w-[700px] text-slate-300/80 md:text-xl mt-4">
