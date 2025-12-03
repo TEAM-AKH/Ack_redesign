905988bcc884a77006ad4ba26e1845139681f73b
Rahul Varma <rahulvarmaviit@gmail.com>
2025-12-03 13:06:58 +0000

    remove this (_for element h1_)

--- src/components/ui/stacking-card.tsx
+++ src/components/ui/stacking-card.tsx
@@ -141,9 +141,6 @@ const Component = forwardRef<HTMLElement, ComponentRootProps>(({ projects }, ref
         </section>
 
         <footer className='group bg-slate-950'>
-          <h1 className='text-[16vw] translate-y-20 leading-[100%] uppercase font-semibold text-center bg-gradient-to-r from-gray-400 to-gray-800 bg-clip-text text-transparent transition-all ease-linear'>
-            ui-layout
-          </h1>
           <div className='bg-black h-40 relative z-10 grid place-content-center text-2xl rounded-tr-full rounded-tl-full'></div>
         </footer>
       </main>
